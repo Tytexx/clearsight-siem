13e5dab76c2604605e085163c5d9ad855a32be2d
Tytexx <mishalashraf37@gmail.com>
2026-01-10 18:35:37 +0300

    change output format

--- code/homelab-siem.py
+++ code/homelab-siem.py
@@ -37,10 +37,17 @@ def detect_windows_bruteforce(event):
         severity = "HIGH" if len(unique_users) > 1 else "MEDIUM"
         alerted_keys.add(ip)
 
-        print(f"\n[{severity}] Windows Brute Force Detected")
-        print(f"IP Address: {ip}")
-        print(f"Username: {event['username']}")
-        print(f"Attempts: {len(failed_login_counter[ip])}\n")
+        alert = {
+            "Severity": severity,               
+            "Title": "Windows Password Spraying Detected",
+            "Source": event["source"],            
+            "IP Address": ip,
+            "Username": event["username"],
+            "Attempts": len(failed_login_counter.get(ip, [])),  
+            "Time Window": "5 minutes",         
+            "Timestamp": event["timestamp"]
+        }
+        emit_alert(alert)
 
 def detect_windows_password_spraying(event):
     if event["source"] != "windows":
@@ -66,15 +73,27 @@ def detect_windows_password_spraying(event):
     if len(unique_ips) >= 3 and username not in alerted_keys:
 
         alerted_keys.add(username)
-        print("[HIGH] Windows Password Spraying Detected")
-        print(f"Targeted Username : {username}")
-        print(f"Unique IPs        : {len(unique_ips)}")
-        print(f"Time Window       : {window}\n")
+        alert = {
+            "Severity": "HIGH",               
+            "Title": "Windows Password Spraying Detected",
+            "Source": event["source"],            
+            "IP Address": ip,
+            "Username": event["username"],
+            "Attempts": len(failed_login_user.get(username, [])),  
+            "Time Window": "3 minutes",         
+            "Timestamp": event["timestamp"]
+        }
+        emit_alert(alert)
 
 def normalize_timestamp(ts):
     dt = datetime.strptime(ts, "%m/%d/%Y %H:%M:%S")
     return dt.isoformat() + "Z"
     print(dt)
+
+def emit_alert(alert):
+    print("\n ALERT")
+    for k,v in alert.items():
+        print(f"{k}: {v}")
 
 def parse_windows_log(line):
     pattern = (
@@ -94,6 +113,7 @@ def parse_windows_log(line):
 def run_detections(event):
     detect_windows_bruteforce(event)
     detect_windows_password_spraying(event)
+
 
 
 with open(r"logs\windowsLogs.txt","r") as logText:
